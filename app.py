c76152f571506a3e7ac2ce417f984ba3b5cdf7a8
Natalia Szczepanek <nszczepanek@op.pl>
2020-05-11 20:15:47 +0200

    [#2] Fixed some mistakes in code.

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 #! /usr/bin/python
 
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask import Flask, render_template
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wyniki.db'
-#
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-# manager = Manager(app)
-#
-# manager.add_command('db', MigrateCommand)
-
 
 @app.route('/')
 def index():
